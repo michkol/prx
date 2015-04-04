73897627fe1c5cc052583ba08e9026731769aed2
michkol <colin@colin.net.pl>
2015-04-04 23:39:32 +0200

    Lepsza walidacja numeru strony.

--- prx_aplikacja/views.py
+++ prx_aplikacja/views.py
@@ -1,17 +1,9 @@
 from django.shortcuts import render
 from django.db.models import Case, When, F
 from .models import BramkaProxy
+import math
 
 def index(zadanie, strona):
-    if strona in [None, '']:
-        strona = 1
-    else:
-        strona = int(strona)
-
-    na_strone = 80
-    pierwszy = 80 * (strona - 1)
-    ostatni = pierwszy + 80
-
     obiekty = BramkaProxy.objects.order_by(
         # szybkie polskie nad innymi
         Case(When(kraj='PL', ping__lt=250, then=1), default=0).desc(),
@@ -27,6 +19,19 @@ def index(zadanie, strona):
         # kolejnych bramek z powtórzonym IP
         (F('ping') * F('ip_indeks') + 85 * (F('ip_indeks') - 1)).asc()
     )
+
+    na_strone = 80
+    ile_stron = math.ceil(obiekty.count() / na_strone)
+
+    if (strona in [None, '']) or (int(strona) < 1):
+        strona = 1
+    elif int(strona) > ile_stron:
+        strona = ile_stron
+    else:
+        strona = int(strona)
+
+    pierwszy = na_strone * (strona - 1)
+    ostatni = pierwszy + na_strone
 
     kontekst = {
         'lista': obiekty[pierwszy:ostatni],
