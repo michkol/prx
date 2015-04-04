0cc10fadd0fdb0986c1251d3cec34e11be8af4ab
michkol <colin@colin.net.pl>
2015-04-04 11:52:13 +0200

    Strona główna - podstawowe funkcje.

--- prx/urls.py
+++ prx/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -7,4 +8,8 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^', include('prx_aplikacja.urls')),
 ]
+
+urlpatterns += static('/', document_root='prx_aplikacja/static')
--- prx_aplikacja/views.py
+++ prx_aplikacja/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import BramkaProxy
 
-# Create your views here.
+def index(zadanie):
+    kontekst = {
+        'lista': BramkaProxy.objects.all()[:80]
+    }
+    return render(zadanie, 'prx_aplikacja/index.html', kontekst)
