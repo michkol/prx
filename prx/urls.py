95e06b48b6c0722882a66855c016794234f4f15e
michkol <colin@colin.net.pl>
2015-04-07 23:21:03 +0200

    Obsługa tylko istniejących plików przez widok static.

--- prx/urls.py
+++ prx/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from .settings import BASE_DIR
+import glob
+import re
 
 urlpatterns = [
     # Examples:
@@ -12,4 +15,18 @@ urlpatterns = [
     url(r'^', include('prx_aplikacja.urls')),
 ]
 
-urlpatterns += static('/', document_root='prx_aplikacja/static')
+# udostępnianie wszystkich plików z folderu static
+# np. URL /style.css -> plik prx_aplikacja/static/style.css
+folder = 'prx_aplikacja/static'
+sciezka = BASE_DIR + '/' + folder
+for plik in glob.glob(sciezka + '/*'):
+    # względem folderu static, a nie bezwzględne
+    plik = plik[len(sciezka) + 1:]
+    
+    urlpatterns.append(
+        url(
+            r'^(?P<path>' + re.escape(plik) + ')$',
+            'django.views.static.serve',
+            {'document_root': folder}
+        )
+    )
